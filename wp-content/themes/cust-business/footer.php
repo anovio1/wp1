012b1a11da8dfaebbbcdf52ad38a3b6db194ea40
anovio1 <35636483+anovio1@users.noreply.github.com>
2020-09-23 03:53:51 -0400

    iconUpload

--- wp-content/themes/cust-business/footer.php
+++ wp-content/themes/cust-business/footer.php
@@ -2,9 +2,9 @@
 
 <div class="broch-footer">
     Connect
-    <a href="" class="connect-icon"><img src="" alt="" class=""></img></a>
-    <a href="" class="connect-icon"><img src="" alt="" class=""></img></a>
-    <a href="" class="connect-icon"><img src="" alt="" class=""></img></a>
+    <a href="" class="connect-icon"><img src="/wp-content/images/logo_instagram.png" alt="" class=""></img></a>
+    <a href="" class="connect-icon"><img src="/wp-content/images/logo_twitter.png" alt="" class=""></img></a>
+    <a href="" class="connect-icon"><img src="/wp-content/images/logo_linkedin.png" alt="" class=""></img></a>
     <span>© 2020 Weyland-Yutani. All Rights Reserved</span>
 </div>
 </body>
